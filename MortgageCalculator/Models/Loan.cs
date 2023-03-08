2c27b1f11b37894398865b184eaa6af14fbd490f
Mansor Almossa <malmossa@gmail.com>
2023-03-08 10:12:00 -0800

    completed

--- MortgageCalculator/Models/Loan.cs
+++ MortgageCalculator/Models/Loan.cs
@@ -4,5 +4,10 @@
     {
         public decimal Amount { get; set; }
         public decimal Rate { get; set; }
+        public int Term { get; set; }
+        public decimal Payment { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<LoanPayment> Payments { get; set; } = new List<LoanPayment>();
     }
 }
